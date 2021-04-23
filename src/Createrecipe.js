d7336302c7b309d8f4298491dd1906cada2b87ac
djakovljevic <djakovljevic@ebay.com>
2021-04-23 14:42:35 +0200

    enable creation of my recipes

--- src/Createrecipe.js
+++ src/Createrecipe.js
@@ -2,62 +2,88 @@ import React, { useState } from "react";
 import "./App.css";
 
 const CreateRecipes = () => {
+  const [title, setTitle] = useState("");
+  const [ingredients, setIngredients] = useState("");
+  const [calories, setCalories] = useState("");
+  const [image, setImage] = useState("");
+  const [myRecipes, setMyRecipes] = useState([]);
 
-    const [title, setTitle] = useState ('');
-    const [ingredients, setIngredients] = useState ('');
-    const [calories, setCalories] = useState ('');
-    const [image, setImage] = useState ('');
+  const updateTitle = (e) => {
+    setTitle(e.target.value);
+  };
 
-    const updateTitle = e => {
-        setTitle(e.target.value);
-    }
+  const updateIngredients = (e) => {
+    setIngredients(e.target.value);
+  };
 
-    const updateIngredients = e => {
-        setIngredients(e.target.value);
-    }
+  const updateCalories = (e) => {
+    setCalories(e.target.value);
+  };
 
-    const updateCalories = e => {
-        setCalories(e.target.value);
-    }
+  const updateImage = (e) => {
+    setImage(e.target.value);
+  };
 
-    const updateImage = e => {
-        setImage(e.target.value);
-    }
+  const addRecipe = (e) => {
+    e.preventDefault();
 
-    const addRecipe = e => {                                                                                                                                                                                                                                               
-        e.preventDefault();
-    }
+    setMyRecipes((myRecipes) => [
+      ...myRecipes,
+      { title, ingredients, calories, image },
+    ]);
 
-    return(
-        <div className="box">
-            <div className="create-box">
-                <form onSubmit={addRecipe}>
-                    <br/><br/>
-                    <h3>Create your recipe</h3>
-                    <p>Recipe name:</p>
-                    <input type="text" name="name" value={title} onChange={updateTitle}/>
-                    <p>Ingrediens:</p>
-                    <input type="text" name="ingredients" value={ingredients} onChange={updateIngredients}/>
-                    <p>Calories:</p>
-                    <input type="text" name="calories" value={calories} onChange={updateCalories}/>
-                    <p>Image adress:</p>
-                    <input type="text" name="img" value={image} onChange={updateImage}/><br/><br/>
-                    <button className="submit-button" type="submit">Submit</button><br/>
-                </form>
-            </div>
-            <div className="new-recipe">
-                <form>
-                    <h1>{title}</h1>
-                    <ol>
-                        {ingredients}
-                    </ol>
-                    <p>{calories}</p>
-                    <img src={image} className="new-image"/>
-                    <br/>
-                </form>
-            </div>
+    setTitle("");
+    setIngredients("");
+    setCalories("");
+    setImage("");
+  };
+
+  console.log(myRecipes);
+
+  return (
+    <div className="box">
+      <div className="create-box">
+        <form onSubmit={addRecipe}>
+          <br />
+          <br />
+          <h3>Create your recipe</h3>
+          <p>Recipe name:</p>
+          <input type="text" name="name" value={title} onChange={updateTitle} />
+          <p>Ingrediens:</p>
+          <input
+            type="text"
+            name="ingredients"
+            value={ingredients}
+            onChange={updateIngredients}
+          />
+          <p>Calories:</p>
+          <input
+            type="text"
+            name="calories"
+            value={calories}
+            onChange={updateCalories}
+          />
+          <p>Image adress:</p>
+          <input type="text" name="img" value={image} onChange={updateImage} />
+          <br />
+          <br />
+          <button className="submit-button" type="submit">
+            Submit
+          </button>
+          <br />
+        </form>
+      </div>
+      {myRecipes.map((recipe) => (
+        <div className="new-recipe">
+          <h1>{recipe.title}</h1>
+          <ol>{recipe.ingredients}</ol>
+          <p>{recipe.calories}</p>
+          <img src={recipe.image} alt="new recipe" className="new-image" />
+          <br />
         </div>
-    );
-}
+      ))}
+    </div>
+  );
+};
 
 export default CreateRecipes;
--- src/Nav.js
+++ src/Nav.js
@@ -3,22 +3,21 @@ import "./App.css";
 import { Link } from "react-router-dom";
 
 function Nav() {
-    const navStyle = {
-        color: "black"
-    };
-    return(
-        <nav>
-            <h3 className="logo">FOOD</h3>
-            <ul className="nav-items">
-                <Link style={navStyle} to="/">
-                <li>HOME</li>
-                </Link>
-                <Link style={navStyle} to="/createrecipe">
-                <li>CREATE RECIPE</li>
-                </Link>
-            </ul>
-        </nav>
-    )
+  const navStyle = {
+    color: "black",
+  };
+  return (
+    <nav>
+      <h3 className="logo">FOOD</h3>
+      <ul className="nav-items">
+        <Link style={navStyle} to="/">
+          <li>HOME</li>
+        </Link>
+        <Link style={navStyle} to="/createrecipe">
+          <li>CREATE RECIPE</li>
+        </Link>
+      </ul>
+    </nav>
+  );
 }
 export default Nav;
-
